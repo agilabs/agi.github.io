6f16d2cb72bb518797780e863b41e9bb4a88ca4a
Ethan Lee <>
2022-12-28 23:14:20 +0900

    no message

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
 
 //!
-var menuSubs = [].slice.call(document.querySelectorAll('.dropdown-menu')),
+var menuItems = [].slice.call(document.querySelectorAll('.menu__item')),
+    menuSubs = [].slice.call(document.querySelectorAll('.dropdown-menu')),
     selectedMenu = undefined,
     subBg = document.querySelector('.dropdown__bg'),
     subBgBtm = document.querySelector('.dropdown__bg-bottom'),
@@ -26,6 +27,8 @@ var menuSubs = [].slice.call(document.querySelectorAll('.dropdown-menu')),
         var subMeta = menuSubCnt.getBoundingClientRect();
 
         selectedMenu = menuId;
+
+        menuItems.forEach(el => el.classList.remove('active'));
 
         el.classList.add('active');
 
@@ -51,6 +54,7 @@ var menuSubs = [].slice.call(document.querySelectorAll('.dropdown-menu')),
         header.classList.add('dropdown-active');
     },
     closeDropdown = function () {
+        menuItems.forEach(el => el.classList.remove('active'));
         menuSubs.forEach(el => {
             el.classList.remove('active');
             el.style.opacity = 0;
@@ -62,15 +66,15 @@ var menuSubs = [].slice.call(document.querySelectorAll('.dropdown-menu')),
         header.classList.remove('dropdown-active');
     };
 
-// menuItems.forEach(el => {
+menuItems.forEach(el => {
 
-//     el.addEventListener('mouseenter', function () {
-//     stopCloseTimeout();
-//     openDropdown(this);
-// }, false);
+    el.addEventListener('mouseenter', function () {
+    stopCloseTimeout();
+    openDropdown(this);
+}, false);
 
-//     el.addEventListener('mouseleave', () => startCloseTimeout(), false);
-// });
+    el.addEventListener('mouseleave', () => startCloseTimeout(), false);
+});
 
 menuSubs.forEach(el => {
     el.addEventListener('mouseenter', () => stopCloseTimeout(), false);
